2591302ff6f8da4e16aafc8a537be26ebf609700
Bajt0l <121569965+Bajt0l@users.noreply.github.com>
2023-03-12 10:28:29 +0100

    Add files via upload

---
+++ reportSystem/config.lua
@@ -0,0 +1,25 @@
+Config = {}
+Config.commandMember = 'zglos'
+Config.commandAdmin = 'zgloszenia'
+
+Config.TranslationWords = {
+    title = 'System Reportow',
+    report = {
+        noReport = 'Obecnie nie ma żadnych reportów!',
+        reportBadgeTitle = 'Zgłos:',
+        removeButton = 'Usun',
+        searchPlaceholder = 'Wyszukaj [ID] lub [Gracz]'
+    },
+    alert = {
+        newReport = 'Nowy report',
+        success = "<span class='font-weight-bold font-italic text-primary'>Pomyślnie</span> wysłałeś report!",
+        solved = "Zgłoszenie <span class='font-weight-bold font-italic text-primary'>rozwiązane!</span>",
+        error = "Zgłoszenie <span class='font-weight-bold font-italic' style='color: var(--error)'>odrzucone!</span>",
+        toastAdminRemoved = "<span class='font-weight-bold font-italic text-primary'>usunąłeś report!</span>",
+        toastAdminSolved = "<span class='font-weight-bold font-italic text-primary'>rozwiązałeś report!</span>",
+        toastGoto =  "Pomyślnie się teleportowałeś",
+        toastBring =  "Udało ci się go tutaj sprowadzić",
+        toastGotoPlayer = 'teleportował się tutaj!',
+        toastBringPlayer = 'przywieziony tutaj!',
+    },
+}
